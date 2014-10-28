c38cab9f06b9a75f4a49103349249b1360078824
hecj <275070023@qq.com>
2014-10-29 00:36:41 +0800

    ssh maven

--- src/test/java/com/test/services/spring/SpringTest.java
+++ src/test/java/com/test/services/spring/SpringTest.java
@@ -3,18 +3,23 @@ package com.test.services.spring;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hecj.dao.dao.StudentDao;
+
 
 public class SpringTest {
 	public static void main(String[] args) {
-/*
+
 		// 读取配置文件
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContent.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("daoBeans.xml");
 		// 查找bean
 
 		StudentDao studentDao = (StudentDao) ctx.getBean("studentDao");
 
 		List list = studentDao.queryListStudnet();
 		System.out.println(list.size());
-*/
+
 	}
 }
